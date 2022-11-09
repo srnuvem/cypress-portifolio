883311fbc7b8b0e8e97a2f700ec517d8de6b502a
p <p@pop-os.localdomain>
2022-11-09 00:14:54 -0300

    CyP-0001 Cypress configuration, First custom command and First User Journey

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,6 @@
 /// <reference types="cypress" />
 
 //Adding syntactic sugar for getting elements by data-test
-
 Cypress.Commands.add("getByData", (selector) => {
     return cy.get(`[data-test=${selector}]`)
 })
